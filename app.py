e4929e7b97958189ed73571d2a516492f8d485b0
ANHAD SINGH BHATIA <94342611+anhadcode@users.noreply.github.com>
2023-07-20 08:47:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,8 +72,7 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 
 st.write("#")
 st.subheader("Education Qualification")
-st.write(
-    """
+st.write("""
 #### BSc Physics (Hons): University of Delhi
 - Scientific Computation with Python
 - Mathematical Physics
@@ -81,9 +80,7 @@ st.write(
 #### Certication Data Analytics: YMCA Delhi
 - Statistics, and Microsoft Access
 #### Certification Data Analytics: SSCBS University of Delhi
-- Advanced Statistics for Data Analytics, Business Intelligence and Machine Learning
-    """
-)
+- Advanced Statistics for Data Analytics, Business Intelligence and Machine Learning """)
 
 st.subheader("Tools and Skills")
 columns = st.columns(4)
